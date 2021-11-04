f20f050a2953d3304b9726301ee1049c7f4d37b9
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-11-05 03:50:07 +0700

    Normalize player speed

--- Assets/Scripts/RTSPlayerController2D.cs
+++ Assets/Scripts/RTSPlayerController2D.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 
 public class RTSPlayerController2D : MonoBehaviour
 {
+    public float speed = 1f;
+    
     private Camera _camera;
     private Transform _transform;
+    private float _zPosition;
 
     private void Awake()
     {
         _camera = Camera.main;
-        _transform = gameObject.transform;
+        _transform = transform;
+        _zPosition = _transform.position.z;
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            _transform.DOMove(CursorWorldPosition(), 2f);
+            Vector3 point = CursorWorldPosition();
+            _transform.DOMove(point, TrueSpeed(point));
         }
 
         if (Input.GetKeyDown(KeyCode.S))
@@ -30,6 +35,11 @@ public class RTSPlayerController2D : MonoBehaviour
         Vector2 screenPosition = Input.mousePosition;
         Vector3 worldPosition = _camera.ScreenToWorldPoint(screenPosition);
 
-        return new Vector3(worldPosition.x, worldPosition.y, 1f);
+        return new Vector3(worldPosition.x, worldPosition.y, _zPosition);
+    }
+
+    private float TrueSpeed(Vector3 newVector3)
+    {
+        return Vector3.Distance(_transform.position, newVector3) / speed;
     }
 }
