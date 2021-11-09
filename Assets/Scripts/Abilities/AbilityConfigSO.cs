045e12f1e988903470a42811b0c6a23c4255f96d
TrickyTriangles <47727993+TrickyTriangles@users.noreply.github.com>
2021-11-09 13:06:19 -0500

    ...

---
+++ Assets/Scripts/Abilities/AbilityConfigSO.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Scriptable Objects/Ability/Ability Config")]
+public class AbilityConfigSO : ScriptableObject
+{
+}
