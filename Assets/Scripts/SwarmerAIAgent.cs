58a1ce3ec8b5b9de8b2e725e8bdd56a11bc86ba1
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-11-10 04:36:41 +0700

    Implement utility AI on SwarmerAIAgent.cs

--- Assets/Scripts/SwarmerAIAgent.cs
+++ Assets/Scripts/SwarmerAIAgent.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SwarmerAIAgent : Agent
 {
+    private Vector3 _playerPosition;
+
     private void OnEnable()
     {
         EventManager.StartListening("OnPlayerPositionChanged", OnPlayerPositionChanged);
@@ -14,6 +18,34 @@ public class SwarmerAIAgent : Agent
 
     private void OnPlayerPositionChanged(object newPosition)
     {
-        agent.SetDestination((Vector3)newPosition);
+        _playerPosition = (Vector3)newPosition;
+        InvokeBestAction();
+    }
+
+    private void InvokeBestAction()
+    {
+        // Calculate utility value
+        float chasePlayerUtility = 2 - Vector3.Distance(transform.position, _playerPosition);
+        float stopUtility = 0;
+
+        // Invoke the highest valued utility Action
+        List<(Action, float)> list = new List<(Action, float)>(0)
+        {
+            (ChasePlayer, chasePlayerUtility),
+            (Stop, stopUtility)
+        };
+
+        list.Sort((x, y) => y.Item2.CompareTo(x.Item2));
+        list[0].Item1.Invoke();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(_playerPosition);
+    }
+
+    private void Stop()
+    {
+        agent.SetDestination(transform.position);
     }
 }
