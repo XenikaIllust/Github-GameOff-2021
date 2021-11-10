423367fba496fd6fe5ce52f94ad58d382212512c
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-11-11 02:06:28 +0700

    Add attack action and stats to SwarmerAIAgent.cs

--- Assets/Scripts/SwarmerAIAgent.cs
+++ Assets/Scripts/SwarmerAIAgent.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class SwarmerAIAgent : Agent
 {
+    [Header("Stats")] public float hostileRange = 5f;
+    public float attackRange = 1f;
+    public float attackBufferRange = 2f;
+    public int attackDamage = 1;
+    public float attackSpeed = 0.5f;
+
+    private bool _isBusy;
     private Vector3 _playerPosition;
+    private GameObject _playerGameObject;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _playerGameObject = FindObjectOfType<PlayerAgent>().gameObject;
+    }
 
     private void OnEnable()
     {
@@ -24,13 +38,19 @@ public class SwarmerAIAgent : Agent
 
     private void InvokeBestAction()
     {
+        if (_isBusy) return;
+
+        float distanceFromPlayer = Vector3.Distance(transform.position, _playerPosition);
+
         // Calculate utility value
-        float chasePlayerUtility = 2 - Vector3.Distance(transform.position, _playerPosition);
+        float attackPlayerUtility = float.PositiveInfinity * (attackRange - distanceFromPlayer);
+        float chasePlayerUtility = hostileRange - distanceFromPlayer;
         float stopUtility = 0;
 
         // Invoke the highest valued utility Action
-        List<(Action, float)> list = new List<(Action, float)>(0)
+        List<(Action, float)> list = new List<(Action, float)>
         {
+            (AttackPlayer, attackPlayerUtility),
             (ChasePlayer, chasePlayerUtility),
             (Stop, stopUtility)
         };
@@ -39,15 +59,34 @@ public class SwarmerAIAgent : Agent
         list[0].Item1.Invoke();
     }
 
+    private void AttackPlayer()
+    {
+        // Start attack animation
+        _isBusy = true;
+
+        Stop();
+        Invoke(nameof(AttackPlayerFinish), attackSpeed);
+    }
+
+    private void AttackPlayerFinish()
+    {
+        // Finish attack animation
+        _isBusy = false;
+
+        if (Vector3.Distance(_playerGameObject.transform.position, transform.position) <= attackBufferRange)
+        {
+            EventManager.RaiseEvent("OnPlayerAttacked", attackDamage);
+            Debug.Log($"Player received {attackDamage} damage");
+        }
+    }
+
     private void ChasePlayer()
     {
-        print("ChasePlayer invoked!");
         unitEventHandler.RaiseEvent("OnMoveOrderIssued", _playerPosition);
     }
 
     private void Stop()
     {
-        print("Stop invoked!");
         unitEventHandler.RaiseEvent("OnMoveOrderIssued", transform.position);
     }
 }
