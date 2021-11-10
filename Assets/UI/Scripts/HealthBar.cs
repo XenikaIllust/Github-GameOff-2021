c83203f953ccedf8b0d1e3ecf8306211284b3099
Reuben John B. Unicruz <reubenunicruz@yahoo.com>
2021-11-09 22:16:55 -0500

    Rebuilt and made the Action Bar as a prefab
    Action bar remade and improved upon. Also packed it as a prefab.

---
+++ Assets/UI/Scripts/HealthBar.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
