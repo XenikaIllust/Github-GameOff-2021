7abc56c9320618b9b1f79ca553e7de9395e322f3
rin6321910 <rin6321910@gmail.com>
2021-11-11 21:44:50 +0800

    Delete 2 variables not used

--- Assets/Scripts/Animation/IdleAnimationController.cs
+++ Assets/Scripts/Animation/IdleAnimationController.cs
@@ -10,22 +10,17 @@ public class IdleAnimationController : MonoBehaviour{
     private int degree; //can delete this , just for debug
     [SerializeField]
     private int degreeVariation;
-    private int minDegree;
-    private int maxDegree;
     void Awake(){
-        minDegree = 0;
-        maxDegree = 360;
         degreeVariation = 10;
         _anim = gameObject.GetComponent<Animator>();
         _camera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update(){
         degree = getDegree(MousePosition(_camera)); //can delete this , just for debug
-        _anim.Play(getDegree(MousePosition(_camera))+"_PlayerIdle");
-    }
+        _anim.Play(getDegree(MousePosition(_camera))+"_PlayerIdle"); 
 
+    }
     private Vector3 MousePosition(Camera _camera){
         return _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,_camera.transform.position.z));
     }
@@ -33,17 +28,10 @@ public class IdleAnimationController : MonoBehaviour{
     private int getDegree(Vector3 mousePosition){
         
         Vector3 direction = mousePosition - transform.position;
-        float angle = Mathf.Atan2(direction.x,direction.y) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
         float anglefixed = angle < 0 ? (360.0f + angle) : angle;
         anglefixed = Mathf.Round((anglefixed/degreeVariation)) * degreeVariation;
         anglefixed = anglefixed < 360 ? anglefixed : 0;
-        
-        /*
-        Debug.DrawRay(transform.position, direction, Color.green);
-        if(Input.GetMouseButtonDown(0)){
-            print("angle:"+anglefixed);
-        }
-        */
 
         return (int)anglefixed;
     }
