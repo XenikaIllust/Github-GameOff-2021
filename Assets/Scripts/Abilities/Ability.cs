66112c2049fccc176f7b78e773281a9bb1a10cb4
Zachary Zhao Xing Lim <18150357+XenikaIllust@users.noreply.github.com>
2021-11-11 08:53:51 -0800

    Added animation testing scene

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability : MonoBehaviour
+[CreateAssetMenu(fileName = "Ability", menuName = "Scriptable Objects/Ability/Ability SO")]
+public class Ability : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float Duration;
+    public float Cooldown;
+    public Outcome[] Outcomes;
 }
