405c87c3f7de5fe0a6aef1b996500eabeb5a252a
rin6321910 <rin6321910@gmail.com>
2021-11-13 01:50:28 +0800

    Revision IdleAnimationController2.cs
    and delete IdleAnimationController.cs

--- Assets/Scripts/Animation/IdleAnimationController2.cs
+++ Assets/Scripts/Animation/IdleAnimationController2.cs
@@ -2,33 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleAnimationController2 : MonoBehaviour{
+public class IdleAnimationController2 : MonoBehaviour
+{
     EventProcessor unitEventHandler;
+    [SerializeField]
     private Animator _anim;
+    [SerializeField]
+    private AnimationClip[] _animIdleClip;
+    private AnimationClip OriginalClip ; // hardcode
+    private AnimatorOverrideController aoc;
     private Camera _camera;
-    
+    private int degreeClipLength;
     [SerializeField]
     private int degreeVariation;
-    private void Awake(){
+
+    private void Awake()
+    {
         //unitEventHandler = GetComponent<UnitEventManager>().UnitEventHandler;
-        degreeVariation = 10;
+        degreeClipLength = 36;
+        degreeVariation = 10;        
         _anim = gameObject.GetComponent<Animator>();
         _camera = Camera.main;
+        _animIdleClip = new AnimationClip[degreeClipLength];
+
+        loadResources();
+
+        OriginalClip = _animIdleClip[0];// hardcode
+        aoc = new AnimatorOverrideController(_anim.runtimeAnimatorController);
+        _anim.runtimeAnimatorController = aoc;
     }
     private void Start()
     {
         unitEventHandler = GetComponent<UnitEventManager>().UnitEventHandler;// move to here
-        
         unitEventHandler.StartListening("OnMoveOrderIssued", OnMoveOrderIssued);
     }
 
     private void OnMoveOrderIssued(object destination)
     {
-        _anim.Play(getDegree((Vector3)(destination))+"_PlayerIdle"); 
+        var anims = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+        anims.Add(new KeyValuePair<AnimationClip, AnimationClip>(OriginalClip, _animIdleClip[getDegree((Vector3)(destination)) / 10]));// every clip 10 degree 
+        aoc.ApplyOverrides(anims);
     }
 
-    private int getDegree(Vector3 mousePosition){
-        
+    private int getDegree(Vector3 mousePosition)
+    {
         Vector3 direction = mousePosition - transform.position;
         float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
         float anglefixed = angle < 0 ? (360.0f + angle) : angle;
@@ -36,5 +53,11 @@ public class IdleAnimationController2 : MonoBehaviour{
         anglefixed = anglefixed < 360 ? anglefixed : 0;
 
         return (int)anglefixed;
+    }
+    private void loadResources()
+    {
+        for(int i = 0 ; i < degreeClipLength ; i++)
+            _animIdleClip[i] = (AnimationClip)Resources.Load("Animations/Idle animation clip/"+ i * 10 +"_PlayerIdle");
+        
     }
 }
