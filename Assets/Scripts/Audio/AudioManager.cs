9e7efb8d4207646a9f6deb7f0e3e4ce78930bf62
UnityMember <unitymember9000@gmail.com>
2021-11-13 08:10:35 +0200

    Update AudioManager.cs

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+	public static AudioManager instance; // Singleton
+
 	[SerializeField]
 	private Sound[] sounds;
 
 	private void Awake () {
+		// Only one AudioManager should be present at all times
+		if (instance == null) instance = this;
+		else Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+
+		// Copy all sound attributes across to the corresponding sound source
 		foreach (Sound sound in sounds) {
 			sound.Source = gameObject.AddComponent<AudioSource>();
 			sound.Source.clip = sound.audioClip;
