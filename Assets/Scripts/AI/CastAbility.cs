efb47508e7cf352e7e34615b25991cbe32c05472
Zachary Zhao Xing Lim <18150357+XenikaIllust@users.noreply.github.com>
2021-11-20 13:50:09 -0800

    Revised CLAI.re behavior tree to fire only one ability at a time

--- Assets/Scripts/AI/CastAbility.cs
+++ Assets/Scripts/AI/CastAbility.cs
@@ -6,22 +6,48 @@ using BehaviorDesigner.Runtime.Tasks;
 
 public class CastAbility : Action
 {
+    public int abilityIndex;
     public SharedBool cooldownActive;
-    float coolDown = 5; // hardcoded for testing
+    public SharedBool abilityInUse;
+
+    public float coolDown;
+    public float abilityExecutionTime;
 
     EventProcessor unitEventHandler;
+    Unit self;
+
     public override void OnAwake()
     {
         // cache the player
         unitEventHandler = GetComponent<UnitEventManager>().UnitEventHandler;
+        self = GetComponent<Unit>();
         cooldownActive.Value = false;
     }
 
     public override TaskStatus OnUpdate()
     {
-        unitEventHandler.RaiseEvent("On1stAbilityCasted", null);
+        // unitEventHandler.RaiseEvent("On1stAbilityCasted", null);
+        Debug.Log(abilityIndex + " ability casted!");
+        StartCoroutine(StartAbilityInUseTimer(abilityExecutionTime));
         StartCoroutine(StartCooldown(coolDown));
         return TaskStatus.Success;
+    }
+
+    private IEnumerator StartAbilityInUseTimer(float time) {
+        /*---------------------------------------------------------------------------
+        This function will block execution of other abilities until the current 
+        ability is considered finished. The value executionAbilityTime should be
+        determined from the Ability Scriptable Object, when the ability has finished
+        it's true execution.
+
+        Note: True execution time != Ability duration.
+        Ability duration factors in after effects from main ability execution.
+        Eg. Astral step deals additional damage after. But main control should resume
+        after the teleport and first damage.
+        ---------------------------------------------------------------------------*/
+        abilityInUse.Value = true;
+        yield return new WaitForSeconds(time);
+        abilityInUse.Value = false;
     }
 
     private IEnumerator StartCooldown(float time) { // function for testing
