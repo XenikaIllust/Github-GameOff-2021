787c986d8020310aa1c19cdb4be3cb7fbb8900f5
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-11-21 05:53:18 +0700

    Improve SurvivalMode.cs performance

--- Assets/Scripts/Level Manager/SurvivalMode.cs
+++ Assets/Scripts/Level Manager/SurvivalMode.cs
@@ -9,23 +9,42 @@ public class SurvivalMode : MonoBehaviour
     [SerializeField] private float timer;
 
     private TMP_Text _timerText;
+    private bool _winCondition;
 
     private void Awake()
     {
         popUp.SetActive(false);
         _timerText = timerObject.GetComponentInChildren<TMP_Text>();
+        _winCondition = false;
+        UpdateText();
     }
 
     private void Update()
     {
+        if (_winCondition) return;
+
         if (timer <= float.Epsilon)
         {
+            _winCondition = true;
             popUp.SetActive(true);
-            _timerText.text = TimeSpan.FromSeconds(0).ToString("g");
+            UpdateText();
         }
         else
         {
             timer -= Time.deltaTime;
+            _timerText.text = TimeSpan.FromSeconds(Mathf.FloorToInt(timer)).ToString("g");
+            UpdateText();
+        }
+    }
+
+    private void UpdateText()
+    {
+        if (timer <= float.Epsilon)
+        {
+            _timerText.text = TimeSpan.FromSeconds(0).ToString("g");
+        }
+        else
+        {
             _timerText.text = TimeSpan.FromSeconds(Mathf.FloorToInt(timer)).ToString("g");
         }
     }
