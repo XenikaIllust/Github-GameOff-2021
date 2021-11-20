ec5709d24866a74f85be5207387398a233ef5027
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-11-21 05:46:28 +0700

    Finish "Kill All Enemies" mode prototype

---
+++ Assets/Scripts/Level Manager/KillAllEnemiesMode.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KillAllEnemiesMode : MonoBehaviour
+{
+    [SerializeField] private GameObject popUp;
+    [SerializeField] private GameObject aliveEnemyCounterObject;
+
+    private List<SwarmerAIAgent> _listOfEnemies;
+    private int _aliveEnemyCounter;
+    private TMP_Text _aliveEnemyCounterText;
+
+    private void Awake()
+    {
+        popUp.SetActive(false);
+        _listOfEnemies = new List<SwarmerAIAgent>(FindObjectsOfType<SwarmerAIAgent>());
+        _aliveEnemyCounter = _listOfEnemies.Count;
+        _aliveEnemyCounterText = aliveEnemyCounterObject.GetComponentInChildren<TMP_Text>();
+        UpdateText();
+    }
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("OnUnitDied", OnUnitDied);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("OnUnitDied", OnUnitDied);
+    }
+
+    private void OnUnitDied(object unit)
+    {
+        var unitGameObject = (GameObject)unit;
+
+        if (unitGameObject.GetComponent<SwarmerAIAgent>() != null)
+        {
+            _aliveEnemyCounter -= 1;
+            UpdateText();
+
+            if (_aliveEnemyCounter == 0)
+            {
+                popUp.SetActive(true);
+            }
+        }
+    }
+
+    private void UpdateText()
+    {
+        _aliveEnemyCounterText.text = $"Alive Enemies: {_aliveEnemyCounter}";
+    }
+}
