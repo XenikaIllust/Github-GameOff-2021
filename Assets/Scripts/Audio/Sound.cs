24cbe21a38862a72d5d77e61f714497202bad0a1
UnityMember <unitymember9000@gmail.com>
2021-11-23 11:18:38 +0200

    Update Sound.cs

--- Assets/Scripts/Audio/Sound.cs
+++ Assets/Scripts/Audio/Sound.cs
@@ -4,31 +4,31 @@ using UnityEngine.Audio;
 [System.Serializable]
 public class Sound
 {
-	public string name;
+    public string name;
 
-	public AudioClip audioClip;
-	public AudioMixerGroup audioMixerGroup;
+    public AudioClip audioClip;
+    public AudioMixerGroup audioMixerGroup;
 
-	public bool mute;
-	public bool bypassEffects;
-	public bool bypassListenerEffects;
-	public bool bypassReverbZones;
-	public bool playOnAwake;
-	public bool loop;
+    public bool mute;
+    public bool bypassEffects;
+    public bool bypassListenerEffects;
+    public bool bypassReverbZones;
+    public bool playOnAwake;
+    public bool loop;
 
-	[Range(0, 256)]
-	public int priority;
-	[Range(0.0f,1.0f)]
-	public float volume = 1.0f;
-	[Range(0.1f, 3.0f)]
-	public float pitch = 1.0f;
-	[Range(-1.0f, 1.0f)]
-	public float stereoPan;
-	[Range(0.0f, 1.0f)]
-	public float spatialBlend;
-	[Range(0.0f, 1.1f)]
-	public float reverbZoneMix;
+    [Range(0, 256)]
+    public int priority;
+    [Range(0.0f,1.0f)]
+    public float volume = 1.0f;
+    [Range(0.1f, 3.0f)]
+    public float pitch = 1.0f;
+    [Range(-1.0f, 1.0f)]
+    public float stereoPan;
+    [Range(0.0f, 1.0f)]
+    public float spatialBlend;
+    [Range(0.0f, 1.1f)]
+    public float reverbZoneMix;
 
-	[HideInInspector]
-	public AudioSource Source;
+    [HideInInspector]
+    public AudioSource Source;
 }
