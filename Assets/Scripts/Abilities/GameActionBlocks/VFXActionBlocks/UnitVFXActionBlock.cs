0c3dbd3685e50846cf33b39d788b216301625e54
rin6321910 <rin6321910@gmail.com>
2021-11-27 23:16:09 +0800

    Create Animation And Rough Implement UnitVFXActionBlock

--- Assets/Scripts/Abilities/GameActionBlocks/VFXActionBlocks/UnitVFXActionBlock.cs
+++ Assets/Scripts/Abilities/GameActionBlocks/VFXActionBlocks/UnitVFXActionBlock.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Definitions/Game Action/VFX/UnitVFXActionBlock")]
 public class UnitVFXActionBlock : VFXActionBlock
 {
-    public override void Invoke(string[] idParams, List<object> currentFilteredTargets, Dictionary<string, object> otherTargets) {
-
+    public override void Invoke(string[] idParams, List<object> currentFilteredTargets, Dictionary<string, object> otherTargets)
+    {
+        foreach (object target in currentFilteredTargets)
+        {
+            Unit targetUnit = (Unit)target;
+            Vector2 spawnPoint = (Vector3) otherTargets[idParams[1]]; 
+            ParticleSystem vfx = Instantiate<ParticleSystem>( Resources.Load<ParticleSystem>(libraryPrefix + "UnitVFX/" + idParams[0]), spawnPoint, Quaternion.identity );
+            vfx.Play();
+        }
     }
 }
