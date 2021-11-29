bcd387d63b2e5fbe964e21301c63032cc94c18f7
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-11-30 01:35:47 +0700

    Fix pending cast bug

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -360,7 +360,7 @@ public class Unit : MonoBehaviour
             AbilityInput(_allTargets["Executing Unit Position"]);
         }
 
-        if (Vector3.Distance(transform.position, _castTargetPosition) <= ability.castRange)
+        if (Vector2.Distance(transform.position, _castTargetPosition) <= ability.castRange)
         {
             TurnAndExecuteAbility(ability);
         }
@@ -374,9 +374,9 @@ public class Unit : MonoBehaviour
 
     private IEnumerator PendingCast(Ability ability)
     {
-        while (Vector3.Distance(transform.position, _castTargetPosition) >= ability.castRange)
+        while (Vector2.Distance(transform.position, _castTargetPosition) >= ability.castRange)
         {
-            yield return new WaitForFixedUpdate();
+            yield return null;
             agent.SetDestination(_castTargetPosition);
         }
 
