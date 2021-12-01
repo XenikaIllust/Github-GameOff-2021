8562033d66487fcfcb855cd8a92f228a6717167d
Zachary Zhao Xing Lim <18150357+XenikaIllust@users.noreply.github.com>
2021-11-30 17:54:00 -0800

    Fix cursor size

--- Assets/Scripts/Abilities/TargetFilters/TargetFilter.cs
+++ Assets/Scripts/Abilities/TargetFilters/TargetFilter.cs
@@ -105,7 +105,7 @@ public class TargetFilter
                 AddToList(targets, (Unit)AllTargets["Executing Unit"], unit, hitEnemies, hitAllies, hitSelf);
             }
 
-            GameObject.Destroy(AOECalculator);
+            // GameObject.Destroy(AOECalculator);
         }
         else if (Type == TargetFilterType.TargetsOfPreviousEffect)
         {
